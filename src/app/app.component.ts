362f1162793a80572032b24d9a97c3288554871c
Oscar <oscarcastillog7@gmail.com>
2021-09-01 15:49:56 -0700

    ADD events to dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-select';
+
+  selectedCar: number;
+
+  cars = [
+      { id: 1, name: 'Volvo' },
+      { id: 2, name: 'Saab' },
+      { id: 3, name: 'Opel' },
+      { id: 4, name: 'Audi' },
+      { id: 5, name: 'WV' },
+      { id: 6, name: 'BMW' },
+      { id: 7, name: 'Toyota' },
+      { id: 8, name: 'Honda' },
+      { id: 9, name: 'Ford' },
+  ];
+
+  onChange($event) {
+    console.log($event)
+  }
+
+  onFocus($event: Event) {
+    console.log($event)
+  }
+
+  onBlur($event: Event) {
+    console.log($event)
+  }
+
+  onOpen() {
+    console.log('open')
+  }
+
+  onClose() {
+    console.log('close')
+  }
+
+  onAdd($event) {
+    console.log($event)
+  }
+
+  onRemove($event) {
+    console.log($event)
+  }
+
+  onClear() {
+    console.log('clear')
+  }
+
+  onScrollToEnd($event) {
+    console.log($event)
+  }
+
+  onSearch($event) {
+    console.log($event)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgSelectModule,
+    CommonModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
